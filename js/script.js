350b2ad133d99fb943ad7c87022762317ebfdcec
Litnat Corporation <Litnat Corporation>
2017-01-19 22:22:40 +0300

    Не много изменил архитектуру меню, написал несколько скриптов, поставил релоадер js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+function smoothMovement(navMenu) {
+	$(navMenu).on('click', 'a', function(evt) {
+		evt.preventDefault();
+
+		var idLink = $(this).attr('href');
+		var top = $(idLink).offset().top;
+
+		console.log(idLink);
+		console.log(top);
+
+		$('body,html').animate({
+			scrollTop: top
+		}, 1500);
+	});
+}
+
+function showPortfolio() {
+	var portfolio = $('.portfolio__show');
+
+	portfolio.css({
+		'display': 'none'
+	});
+
+	$('.portfolio__item').hover(function() {
+		$(this).children(2).css('display', 'flex');
+	}, function() {
+		portfolio.css('display', 'none');
+	});
+}
+
+function createStyleMainNavigation() {
+	var mainMenu = $('.page-navigation');
+
+	$(document).scroll(function() {
+		if (mainMenu.offset().top >= 3000) {
+			$('.page-navigation__link').css('color', '#f1f3f7');
+		} else {
+			$('.page-navigation__link').css('color', '#000000');
+			$('.page-navigation__link--start-btn').css('color', '#ffffff');
+		}
+	});
+}
+
+$(document).ready(function() {
+	smoothMovement('.page-navigation');
+	smoothMovement('.footer-nav__navigation');
+	showPortfolio();
+	createStyleMainNavigation();
+});
